f9307584293b77a06ad843766f0a77f1511b6090
Mykolas Stankevicius <mykolas1393@gmail.com>
2024-09-30 11:40:22 +0100

    Dinamically handling templates

--- main.go
+++ main.go
@@ -4,52 +4,64 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
     t, err := template.ParseFiles("templates/" + tmpl + ".html")
     if err != nil {
-        log.Printf("Error loading template %s: %v", tmpl, err) // More detailed logging
+        log.Printf("Error loading template %s: %v", tmpl, err)
         http.Error(w, "Unable to load template", http.StatusInternalServerError)
         return
     }
     err = t.Execute(w, nil)
     if err != nil {
-        log.Printf("Error executing template %s: %v", tmpl, err) // Logging execution errors
+        log.Printf("Error executing template %s: %v", tmpl, err)
         http.Error(w, "Unable to render template", http.StatusInternalServerError)
     }
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "index")
-}
-
-func htmlHandler(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "html")
-}
-
-func cssHandler(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "css")
-}
-
-func jsHandler(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "javascript")
-}
-
-func goHandler(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "go")
+func dynamicHandler(templateName string) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        renderTemplate(w, templateName)
+    }
 }
 
 func main() {
-    http.HandleFunc("/", homeHandler)
-    http.HandleFunc("/html", htmlHandler)
-    http.HandleFunc("/css", cssHandler)
-    http.HandleFunc("/javascript", jsHandler)
-    http.HandleFunc("/go", goHandler)
-
-    // Serve static files
+    // Serve static files (CSS, JS, Images, etc.)
     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
+    // Dynamically register routes based on the template files
+    err := filepath.Walk("templates", func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        // Ignore directories and non-HTML files
+        if !info.IsDir() && strings.HasSuffix(info.Name(), ".html") {
+            // Extract the file name without the .html extension
+            templateName := strings.TrimSuffix(info.Name(), ".html")
+
+            // Generate a route based on the template name
+            route := "/" + templateName
+            if templateName == "index" {
+                route = "/"
+            }
+
+            // Register the dynamic handler for this route
+            http.HandleFunc(route, dynamicHandler(templateName))
+
+            log.Printf("Registered route: %s -> templates/%s.html", route, templateName)
+        }
+        return nil
+    })
+
+    if err != nil {
+        log.Fatalf("Error reading templates directory: %v", err)
+    }
+
+    // Start the server
     log.Println("Server running on http://localhost:8080")
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
